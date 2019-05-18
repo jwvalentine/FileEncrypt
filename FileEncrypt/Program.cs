7bc09e608710c97517830f5bf24aae0d2caaadd4
Joe <jwvalentine@outlook.com>
2019-05-18 10:15:31 -0400

    AES Encryption Fun
    Just playing with the basics of Encryption for now.  Hard coding IV and Key are really terrible dieas.  Don't do this in production code.

--- FileEncrypt/Program.cs
+++ FileEncrypt/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
 
 namespace FileEncrypt
 {
@@ -10,6 +12,87 @@ namespace FileEncrypt
     {
         static void Main(string[] args)
         {
+            string filePath;
+            string input;
+            string key = "dfoishdfiophonsidfoishdfiophonsi"; // 32 characters -- This should be stored in Vault A
+            string iv = "dfoishdfiophonsi"; // 16 characters -- This should be stored in Vault B
+
+            Console.WriteLine("Specifiy the exact path, filename, and extension that you wish to edit:\n(e.g. C:\\EncryptMe\\file.txt)");
+            filePath = Console.ReadLine();
+
+            while (true)
+            {
+                Console.WriteLine("e) Encrypt");
+                Console.WriteLine("d) Decrypt");
+                Console.WriteLine("x) Exit");
+                input = Console.ReadLine();
+
+                if (input == "e")
+                {
+                    EncryptFile(filePath, key, iv);
+                }
+                else if (input == "d")
+                {
+                    DecryptFile(filePath, key, iv);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
         }
-    }
-}
+
+        static void EncryptFile(string filePath, string key, string iv)
+        {
+            byte[] plainContent = File.ReadAllBytes(filePath);
+            using (var AES = new AesCryptoServiceProvider())
+            {
+                AES.BlockSize = 128;
+                AES.KeySize = 256;
+                AES.IV = ASCIIEncoding.ASCII.GetBytes(iv);
+                AES.Key = ASCIIEncoding.ASCII.GetBytes(key);
+                AES.Padding = PaddingMode.PKCS7;
+                AES.Mode = CipherMode.CBC;
+
+                using (var ms = new MemoryStream())
+                {
+                    CryptoStream cryptoStream = new CryptoStream(ms, AES.CreateEncryptor(), CryptoStreamMode.Write);
+
+                    cryptoStream.Write(plainContent, 0, plainContent.Length);
+                    cryptoStream.FlushFinalBlock();
+                    File.WriteAllBytes(filePath, ms.ToArray());
+                    Console.WriteLine("Encrypted Successfully!" + filePath);
+                }
+            }
+        }
+
+
+
+        static void DecryptFile(string filePath, string key, string iv)
+        {
+            byte[] encrypted = File.ReadAllBytes(filePath);
+            using (var AES = new AesCryptoServiceProvider())
+            {
+                AES.BlockSize = 128;
+                AES.KeySize = 256;
+                AES.IV = ASCIIEncoding.ASCII.GetBytes(iv);
+                AES.Key = ASCIIEncoding.ASCII.GetBytes(key);
+                AES.Padding = PaddingMode.PKCS7;
+                AES.Mode = CipherMode.CBC;
+
+                using (var ms = new MemoryStream())
+                {
+                    CryptoStream cryptoStream = new CryptoStream(ms, AES.CreateDecryptor(), CryptoStreamMode.Write);
+
+                    cryptoStream.Write(encrypted, 0, encrypted.Length);
+                    cryptoStream.FlushFinalBlock();
+                    File.WriteAllBytes(filePath, ms.ToArray());
+                    Console.WriteLine("Decrypted Successfully!" + filePath);
+                }
+            }
+
+
+        } // End of Main
+    } // End of Class
+}  // End of Namespace
